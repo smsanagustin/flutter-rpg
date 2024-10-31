9c4d9e2ad5cf3cb5b2e20a67f66daac67c73d628
Sophie San Agustin <smsanagustin@up.edu.ph>
2024-10-31 11:23:15 +0800

    Add images

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,10 +1,18 @@
+import 'package:flutter_rpg/models/skill.dart';
 import 'package:flutter_rpg/models/stats.dart';
+import 'package:flutter_rpg/models/vocation.dart';
 
 class Character with Stats {
   // constructor
-  Character({required this.name, required this.slogan, required this.id});
+  Character(
+      {required this.vocation,
+      required this.name,
+      required this.slogan,
+      required this.id});
 
   // fields
+  final Set<Skill> skills = {};
+  final Vocation vocation;
   final String name;
   final String slogan;
   final String id;
@@ -16,5 +24,10 @@ class Character with Stats {
   // methods
   void toggleIsFav() {
     _isFav = !_isFav;
+  }
+
+  void updateSkill(Skill skill) {
+    skills.clear();
+    skills.add(skill);
   }
 }
