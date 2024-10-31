905394e8847f1587336095a20b4e3acc6f70b398
Sophie San Agustin <smsanagustin@up.edu.ph>
2024-10-31 09:58:20 +0800

    Add stats getters

--- lib/models/stats.dart
+++ lib/models/stats.dart
@@ -5,6 +5,24 @@ mixin Stats {
   int _defense = 10;
   int _skill = 10;
 
+  // getters
+  int get points => _points;
+
+  Map<String, int> get statsAsMap => {
+        "health": _health,
+        "attack": _attack,
+        "defense": _defense,
+        "skill": _skill,
+      };
+
+  List<Map<String, String>> get statsFormattedAsList => [
+        {"title": "health", "value": _health.toString()},
+        {"title": "attack", "value": _attack.toString()},
+        {"title": "defense", "value": _defense.toString()},
+        {"title": "skill", "value": _skill.toString()},
+      ];
+
+  // methods
   increaseStat(String stat) {
     if (_points > 0) {
       if (stat == "health") {
